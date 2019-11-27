8b6b7c6ab9dbc00e9dc7df245c806a1e2d85d975
amanpathak15 <49591094+amanpathak15@users.noreply.github.com>
2019-11-27 17:20:05 +0530

    Update String

--- app/src/main/java/com/mindwarriorhack/app/DeepLinkingActivity.java
+++ app/src/main/java/com/mindwarriorhack/app/DeepLinkingActivity.java
@@ -12,6 +12,6 @@ public class DeepLinkingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_deep_linking);
-        String aa = "asss";
+        String aa = "Ahhh";
     }
 }
